f160eb43a49f5726b50178a5d58c8fe8c6eaf41e
JaGaffney <jagaffney661@msn.com>
2019-06-07 13:31:12 +1000

    youtube ap ptest

--- gtvr/frontend/src/components/lessons/LessonSingle.js
+++ gtvr/frontend/src/components/lessons/LessonSingle.js
@@ -200,7 +200,13 @@ const LessonSingle = props => {
           onChange={(event) => setActiveVolume(parseFloat(event.target.value))}
         />
 
-        <a href={`intent://www.youtube.com/watch?v=${videoIDCode}#Intent;scheme=http;package=com.google.android.apps.youtube.vr;end`}>test</a>     
+        <a href={`intent:
+                    //www.youtube.com/watch?v=${videoIDCode}
+                    #Intent;
+                      scheme=vnd.youtube;
+                      package=com.google.android.youtube;
+                      S.browser_fallback_url=market://details?id=com.google.android.youtube;
+                  end`}>Launch in App</a>     
       </div>  
 
     </div>
